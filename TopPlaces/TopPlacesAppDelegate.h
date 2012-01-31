076545a80ff5f3f2a36c856cdf9ef54a6e262e41
arnaud <arnaud@dragsystem.local>
2012-01-31 15:09:43 +0100

    Initial Commit

---
+++ TopPlaces/TopPlacesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TopPlacesAppDelegate.h
+//  TopPlaces
+//
+//  Created by Arnaud Leene on 31/1/12.
+//  Copyright (c) 2012 MicroContent Musings - Hovering Above. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TopPlaces/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TopPlaces
+//
+//  Created by Arnaud Leene on 31/1/12.
+//  Copyright (c) 2012 MicroContent Musings - Hovering Above. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TopPlacesAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TopPlacesAppDelegate class]));
+    }
+}
